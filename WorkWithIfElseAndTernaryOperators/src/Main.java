4f85fdc8a3109d8433ca32c224e9dbb6bf83ea73
VitaliyOleinik <oleynik624@gmail.com>
2021-03-19 20:56:37 +0600

    ternary operation

---
+++ WorkWithIfElseAndTernaryOperators/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int maximum = (a > b)?a:b;
+        System.out.println(maximum);
+    }
+
+}
